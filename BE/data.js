3ad41f71d2f4da985051119dd167f84f9e93f2f0
JulioFloHub06 <00018824@uca.edu.sv>
2025-10-22 23:30:57 -0600

    FEAT: Agregar BE de Parcial 02 WEB, 00018824

---
+++ BE/data.js
@@ -0,0 +1,66 @@
+export const data = [
+  {
+    "_id": "68f9a9cfb63bf8e29df4119a",
+    "isActive": false,
+    "balance": "$1,364.70",
+    "picture": "http://placehold.it/32x32",
+    "client": "Patti Frost",
+    "gender": "female"
+  },
+  {
+    "_id": "68f9a9cf5f3f4f4e1c3e2b7d",
+    "isActive": true,
+    "balance": "$3,947.21",
+    "picture": "http://placehold.it/32x32",
+    "client": "Lena Mccarty",
+    "gender": "male"
+  },
+  {
+    "_id": "68f9a9cfadeadadc3556da11",
+    "isActive": true,
+    "balance": "$3,142.74",
+    "picture": "http://placehold.it/32x32",
+    "client": "Jacquelyn Decker",
+    "gender": "female"
+  },
+  {
+    "_id": "68f9a9cf922cf608f3fc9fc7",
+    "isActive": true,
+    "balance": "$3,336.40",
+    "picture": "http://placehold.it/32x32",
+    "client": "Zelma Fuller",
+    "gender": "female"
+  },
+  {
+    "_id": "68f9a9cfc342cfc2c5beffbd",
+    "isActive": false,
+    "balance": "$1,144.11",
+    "picture": "http://placehold.it/32x32",
+    "client": "Roberson Wall",
+    "gender": "male"
+  },
+  {
+    "_id": "68f9a9cfb5bb3358d68978b1",
+    "isActive": true,
+    "balance": "$2,099.70",
+    "picture": "http://placehold.it/32x32",
+    "client": "Roseann Santiago",
+    "gender": "female"
+  },
+  {
+    "_id": "68f9a9cf03402b7fa8db4d31",
+    "isActive": true,
+    "balance": "$1,749.19",
+    "picture": "http://placehold.it/32x32",
+    "client": "Parrish Harrington",
+    "gender": "male"
+  },
+{
+    "_id": "68f9a9cf03402b7fa8db4d31",
+    "isActive": true,
+    "balance": "$1,749.19",
+    "picture": "http://placehold.it/32x32",
+    "client": "Parrish Harrington",
+    "gender": "male"
+  }
+]
---
+++ BE/index.js
@@ -0,0 +1,114 @@
+import express from "express";
+import { data } from "./data.js";           
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3130;
+
+
+app.listen(PORT, () => {
+  console.log("Server Listening on PORT: ", PORT);
+});
+
+app.get("/cuentas", (req, res) => {
+  res.json({
+    count: data.length,
+    data
+  });
+});
+
+//Link de prueba
+// http://localhost:3130/cuentas
+
+app.get('/cuenta/:id', (req, res) => {
+  const { id } = req.params;
+  const dat = data.find(d => d._id === String(id));
+
+  if (dat) {
+    res.json({
+      finded: true,
+      account: dat
+    });
+  } else {
+    res.json({
+      finded: false,
+      account: null
+    });
+  }
+});
+
+//Link de prueba valido
+// http://localhost:3130/cuenta/68f9a9cfb63bf8e29df4119a
+
+//Link de prueba no valido
+// http://localhost:3130/cuenta/123456789
+
+app.get('/cuentasParam', (req, res) => {
+  const { queryParam } = req.query
+  
+  if (!queryParam) {
+    return res.json({
+      finded: false,
+      message: "Debe proporcionar un parámetro de búsqueda"
+    });
+  }
+  
+  const resultados = data.filter(dat => {
+    return (
+      dat._id === queryParam ||
+      dat.client.toLowerCase().includes(queryParam.toLowerCase()) ||
+      dat.gender.toLowerCase() === queryParam.toLowerCase()
+    );
+  });
+  
+  if (resultados.length === 0) {
+    return res.json({
+      finded: false
+    });
+  }
+  
+  if (resultados.length === 1) {
+    return res.json({
+      finded: true,
+      account: resultados[0]
+    });
+  }
+  
+  return res.json({
+    finded: true,
+    data: resultados
+  });
+});
+//Links de prueba validos
+// http://localhost:3130/cuentasParam?queryParam=male
+//estos ultimos 2 endpoints son de datos repetidos para verificar que si se tomen en cuenta todos los datos y no solo uno
+// http://localhost:3130/cuentasParam?queryParam=68f9a9cf03402b7fa8db4d31
+// http://localhost:3130/cuentasParam?queryParam=Parrish%20Harrington
+
+
+app.get('/cuentasBalance', (req, res) => {
+  const cuentasActivas = data.filter(dat=> dat.isActive === true);
+  
+  if (cuentasActivas.length === 0) {
+    return res.json({
+      status: false,
+      accountBalance: 0
+    });
+  }
+
+  const balanceTotal = cuentasActivas.reduce((total, dat) => {
+    return total + parseFloat(dat.balance.replace(/[$,]/g, ''));
+  }, 0);
+
+  res.json({
+    status: true,
+    accountBalance: balanceTotal
+  });
+});
+
+//Link de prueba
+//http://localhost:3130/cuentasBalance
+
+
